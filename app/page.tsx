ed5491c04d36d2df69398eb3cef996e73c87550e
cabangilemakertingconsultant-blip <cabangilemakertingconsultant@gmail.com>
2026-05-20 22:29:34 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>SaaS Marketplace Enterprise</h1>
+      <p>Production-ready deploy starter</p>
+    </div>
+  );
+}
