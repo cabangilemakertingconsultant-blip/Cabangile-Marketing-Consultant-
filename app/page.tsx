4d210f534b2875f200f26b1d5c25b757393822d7
cabangilemakertingconsultant-blip <cabangilemakertingconsultant@gmail.com>
2026-05-20 23:53:05 +0200

    Delete page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>SaaS Marketplace Enterprise</h1>
-      <p>Production-ready deploy starter</p>
-    </div>
-  );
-}
